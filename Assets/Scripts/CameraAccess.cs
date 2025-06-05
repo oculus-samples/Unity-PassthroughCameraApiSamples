7319d5731cde9e4aa5488b407b618ba71a51ab66
kkkieuly <lyykieua@gmail.com>
2025-06-05 16:22:38 +0200

    projekt mit git verknüpfen

---
+++ Assets/Scripts/CameraAccess.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using PassthroughCameraSamples;
+using UnityEngine;
+
+public class CameraAccess : MonoBehaviour
+{
+    [SerializeField] private WebCamTextureManager camTextureManager;
+    [SerializeField] private RawImage camImage;
+
+
+}
